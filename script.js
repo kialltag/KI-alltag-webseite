56de105921982832d90e5564c3c3da36cc935bd3
kialltag <kialltag@proton.me>
2025-04-23 15:03:23 +0000

    hanburger menü

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// Header beim Scrollen aus-/einblenden
+let lastScrollY = window.scrollY;
+const header = document.querySelector('header');
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+
+// Header bewegen
+window.addEventListener('scroll', () => {
+  if (window.scrollY > lastScrollY) {
+    header.style.top = "-80px";
+  } else {
+    header.style.top = "0";
+  }
+  lastScrollY = window.scrollY;
+});
+
+// Menü aufklappen
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  menuToggle.classList.toggle('open');
+});
+
+// Glattes Scrollen
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
